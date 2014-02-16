796751c3633d5dd8e7a5fb726bc9db041431afb3
Nicholas Scheurich <nick@scheurich.me>
2014-02-16 16:23:24 -0600

    Allow player to select units

---
+++ Assets/Scripts/SelectUnits.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectUnits : MonoBehaviour {
+	// public Material selectedMaterial;
+	public List<GameObject> selectedUnits;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo = new RaycastHit();
+
+		if (Input.GetMouseButtonDown(0)) {
+			if (Physics.Raycast(ray, out hitInfo) && hitInfo.collider.tag == "Unit") {
+				GameObject obj = hitInfo.collider.gameObject;
+
+			    if (!selectedUnits.Contains(obj)) {
+					if (!Input.GetKey(KeyCode.LeftShift)) ClearSelectedUnits();
+					selectedUnits.Add(obj);
+					obj.renderer.material.color = Color.green;
+				}
+			} else {
+				ClearSelectedUnits();
+			}
+		}
+	}
+
+	void ClearSelectedUnits() {
+		selectedUnits.ForEach(delegate(GameObject o) {
+			o.renderer.material.color = Color.white;
+		});
+		selectedUnits.Clear();
+	}
+}
