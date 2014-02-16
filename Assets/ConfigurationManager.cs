0c18641036d4b9106ba3e4aaf8c47884aeef4790
jkwascom <jkwascom@gmail.com>
2014-02-16 22:31:06 +0000

    add minor scripts and script generator package

---
+++ Assets/ConfigurationManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfigurationManager : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+				Application.runInBackground = true;
+				Debug.Log (Application.loadedLevelName);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
---
+++ Assets/Scripts/World/Mover.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mover : MonoBehaviour {
+	
+	// Start is called just before any of the
+	// Update methods is called the first time.
+	void Start () {
+		
+	}
+	
+	// Update is called every frame, if the
+	// MonoBehaviour is enabled.
+	void Update () {
+		
+	}
+	
+}
