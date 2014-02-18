c009b5b6c981ecb98cc2ea6ec2b38f7cf16cfa9f
jkwascom <jkwascom@gmail.com>
2014-02-18 02:53:45 +0000

    adding basic movement

--- Assets/Scripts/SelectUnits.cs
+++ Assets/Scripts/SelectUnits.cs
@@ -2,39 +2,67 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class SelectUnits : MonoBehaviour {
+public class SelectUnits : MonoBehaviour
+{
 	// public Material selectedMaterial;
 	public List<GameObject> selectedUnits;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hitInfo = new RaycastHit();
+	void Update ()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hitInfo = new RaycastHit ();
 
-		if (Input.GetMouseButtonDown(0)) {
-			if (Physics.Raycast(ray, out hitInfo) && hitInfo.collider.tag == "Unit") {
+		if (Input.GetMouseButtonDown (0)) {
+			if (Physics.Raycast (ray, out hitInfo) && hitInfo.collider.tag == "Unit") {
 				GameObject obj = hitInfo.collider.gameObject;
 
-			    if (!selectedUnits.Contains(obj)) {
-					if (!Input.GetKey(KeyCode.LeftShift)) ClearSelectedUnits();
-					selectedUnits.Add(obj);
+				if (!selectedUnits.Contains (obj)) {
+					if (!Input.GetKey (KeyCode.LeftShift))
+						ClearSelectedUnits ();
+					selectedUnits.Add (obj);
 					obj.renderer.material.color = Color.green;
 				}
 			} else {
-				ClearSelectedUnits();
+				ClearSelectedUnits ();
 			}
 		}
+
+		
+		if (Input.GetMouseButtonDown (1) && selectedUnits.Count > 0) {
+			if (Physics.Raycast (ray, out hitInfo)) {
+				Mover seeker;
+				if (hitInfo.collider.tag == "Unit") {
+					GameObject target = hitInfo.collider.gameObject;
+					selectedUnits.ForEach (delegate(GameObject o) {
+						seeker = (Mover)o.GetComponent<Mover> ();
+						seeker.follow (target);
+					});
+				} else {
+					selectedUnits.ForEach (delegate(GameObject o) {
+						seeker = (Mover)o.GetComponent<Mover> ();
+						seeker.moveTo (hitInfo.point);
+					});
+
+				}
+
+			
+			}
+		}
+
 	}
 
-	void ClearSelectedUnits() {
-		selectedUnits.ForEach(delegate(GameObject o) {
+	void ClearSelectedUnits ()
+	{
+		selectedUnits.ForEach (delegate(GameObject o) {
 			o.renderer.material.color = Color.white;
 		});
-		selectedUnits.Clear();
+		selectedUnits.Clear ();
 	}
 }
--- Assets/Scripts/World/Mover.cs
+++ Assets/Scripts/World/Mover.cs
@@ -2,18 +2,62 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Mover : MonoBehaviour {
+public class Mover : MonoBehaviour
+{
+	Vector3 moveTarget;
+	GameObject followTarget;
+	public float followDistance = 1.0f;
+	public float maximumVelocity = 1.0f;
 	
 	// Start is called just before any of the
 	// Update methods is called the first time.
-	void Start () {
-		
+	void Start ()
+	{
+		if (followTarget == null) {
+			this.follow (this.gameObject);
+		}
+		moveTarget = followTarget.transform.position;
+
 	}
 	
 	// Update is called every frame, if the
 	// MonoBehaviour is enabled.
-	void Update () {
+	void Update ()
+	{
+		try2DMove ();
+	}
+
+	void try2DMove ()
+	{
+		Vector2 moveDirection;
+		Vector3 desiredVelocity = new Vector3 (0, 0, 0);
+
+		if (followTarget != this.gameObject) {
+			moveTarget = followTarget.transform.position;
+		}
+		moveDirection.x = moveTarget.x - this.transform.position.x;
+		moveDirection.y = moveTarget.z - this.transform.position.z;
+
+		transform.eulerAngles = new Vector3 (90, 0, (180 - 360 * Mathf.Atan2 (moveDirection.x, moveDirection.y) / (2*Mathf.PI)));
 		
+		if (moveDirection.sqrMagnitude > (followDistance * followDistance)) {
+			moveDirection.Normalize ();
+			desiredVelocity.x = maximumVelocity * moveDirection.x;
+			desiredVelocity.z = maximumVelocity * moveDirection.y;
+		}
+
+		Vector3 deltaVelocity = desiredVelocity - rigidbody.velocity;
+		rigidbody.AddForce (deltaVelocity, ForceMode.Acceleration);
 	}
 	
+	public void moveTo (Vector3 target)
+	{
+		this.follow (this.gameObject);
+		this.moveTarget = target;
+	}
+	
+	public void follow (GameObject target)
+	{
+		this.followTarget = target;
+	}
 }
