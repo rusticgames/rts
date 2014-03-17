c3788713be7a57b7413deebd67818236eddbe5b0
Nicholas Scheurich <nick@scheurich.me>
2014-03-17 09:34:18 -0500

    Add pausing

---
+++ Assets/Scripts/TimeManagement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManagement : MonoBehaviour {
+	public bool paused = false;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P)) {
+			paused = !paused;
+			Pause(paused);
+		}
+	}
+
+	void OnGUI () {
+		if (paused) {
+			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "Paused");
+		}
+	}
+
+	void Pause (bool paused) {
+		Time.timeScale = paused ? 0.0f : 1.0f;
+		Rigidbody[] rigidbodies = FindObjectsOfType(typeof(Rigidbody)) as Rigidbody[];
+		foreach (Rigidbody rigidbody in rigidbodies) {
+			rigidbody.isKinematic = paused;
+		}
+	}
+}
