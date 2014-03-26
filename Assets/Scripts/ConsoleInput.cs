1fbd7d7e4e44a095b48948bec0385830ec584975
Nicholas Scheurich <nick@scheurich.me>
2014-03-26 15:22:10 -0500

    Make a console thing; it's all-knowing

---
+++ Assets/Scripts/ConsoleInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(UIInput))]
+public class ConsoleInput : MonoBehaviour
+{
+	public UITextList textList;
+
+	private UIInput cli;
+
+	void Start()
+	{
+		cli = GetComponent<UIInput>();
+	}
+
+	void Update()
+	{
+		cli.selected = true;
+	}
+
+	void OnSubmit()
+	{
+		textList.Add(cli.text);
+		if (cli.text == "exit") {
+			GameObject.Find("Trigger").GetComponent<ActivateConsole>().ExitConsole();
+		}
+		cli.text = "";
+	}
+}
