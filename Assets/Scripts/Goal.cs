65dcd374d3a6e26293b891c9812cb0c581efb1e8
Nicholas Scheurich <nick@scheurich.me>
2014-10-07 23:48:33 -0500

    Simple goal set up

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Goal : MonoBehaviour {
+
+	public bool allConditionsMet = false;
+
+	private Component[] conditions;
+
+	void Start () {
+		conditions = GetComponents(typeof(IGoalCondition));
+		StartCoroutine(Check());
+	}
+
+	IEnumerator Check () {
+		while (!allConditionsMet) {
+			for (var i = 0; i < conditions.Length; i++) {
+				if (!((IGoalCondition)conditions[i]).ConditionMet) {
+					allConditionsMet = false;
+					yield return null;
+				} else {
+					allConditionsMet = true;
+				}
+			}
+		}
+
+		Debug.Log("Ya fuckin' won!");
+	}
+}
+
+public interface IGoalCondition
+{
+	bool ConditionMet { get; set; }
+}
---
+++ Assets/Scripts/GoalConditions/CollectAll.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectAll : MonoBehaviour, IGoalCondition {
+
+	public bool ConditionMet { get; set; }
+
+	private GameObject[] collectables;
+
+	void Start () {
+		collectables = GameObject.FindGameObjectsWithTag("Collectable");
+		StartCoroutine(CheckForConditionMet());
+	}
+	
+	IEnumerator CheckForConditionMet () {
+		while (!ConditionMet) {
+			foreach (var c in collectables) {
+				Transform cParent = c.transform.parent;
+				if (cParent == null || cParent.tag != "Player") {
+					ConditionMet = false;
+					yield return null;
+				} else {
+					ConditionMet = true;
+				}
+			}
+		}
+	}
+}
