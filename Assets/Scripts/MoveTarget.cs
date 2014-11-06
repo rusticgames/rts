9cd159cf0322fa21ca346a473b4f5b69b36abd74
Nicholas Scheurich <nick@scheurich.me>
2014-11-06 14:58:13 -0600

    Joystick to world mapping; physics sandbox

---
+++ Assets/Scripts/MoveTarget.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTarget : MonoBehaviour {
+
+	public GameObject mover;
+	public float moverSpeed = 1.0f;
+
+	private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target = transform.position;
+		float step = moverSpeed * Time.deltaTime;
+		mover.transform.position = Vector3.MoveTowards(mover.transform.position, target, step);
+	}
+}
