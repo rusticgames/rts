3293024ab0297ff5b7f5d60d31f2e2893a9b6420
Nicholas Scheurich <nick@scheurich.me>
2014-12-11 11:50:50 -0600

    Add Goal and rolling pin

---
+++ Assets/AreaGoal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaGoal : MonoBehaviour {
+	public UnityEngine.UI.Text text;
+
+	void Start() {
+		text.text = "Reach the goal!";
+	}
+
+	void OnTriggerEnter(Collider other) {
+		// Destroy(other.gameObject);
+		text.text = other.name + " the goal!";
+	}
+}
