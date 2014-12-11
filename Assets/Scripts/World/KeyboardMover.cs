65554c1e11591df76482c103b638610a0b108b8e
Nicholas Scheurich <nick@scheurich.me>
2014-12-11 15:06:30 -0600

    Separate out movement, targeting, and keyboard input

--- Assets/Scripts/World/KeyboardMover.cs
+++ Assets/Scripts/World/KeyboardMover.cs
@@ -4,50 +4,27 @@ using System.Collections.Generic;
 
 public class KeyboardMover : MonoBehaviour {
 
-	public Vector3 moveTarget;
-	public Vector3 newTargetOffset;
 	public float followDistance = .001f;	
-	public float maximumVelocity = 100.0f;
+	public float maximumVelocity = 10.0f;
 	public bool overrideLinear = false;
 	public bool relativeLinear = false;
 	public Vector3 linear;
 	public Vector3 lastLinearForce;
-	public KeyCode up;
-	public KeyCode down;
-	public KeyCode left;
-	public KeyCode right;
+	public KeyboardTargeter targeter;
 
 	private Vector3 distanceVector;
 
-	void Start () {
-		moveTarget = this.rigidbody.position;
-		StartCoroutine(checkInput());
-		StartCoroutine(moveRoutine());
+	void Reset() {
+		targeter = this.GetComponent<KeyboardTargeter>();
 	}
-	
-	IEnumerator checkInput() {
-		while(true) {
-			newTargetOffset = Vector3.zero;
-			if (Input.GetKey (up)) {
-				newTargetOffset.z += .1f;
-			}
-			if (Input.GetKey (down)) {
-				newTargetOffset.z -= .1f;
-			}
-			if (Input.GetKey (left)) {
-				newTargetOffset.x -= .1f;
-			}
-			if (Input.GetKey (right)) {
-				newTargetOffset.x += .1f;
-			}
-			moveTarget = newTargetOffset + this.rigidbody.position;
-			yield return new WaitForFixedUpdate();
-		}
+
+	void Start () {
+		StartCoroutine(moveRoutine());
 	}
 
 	IEnumerator moveRoutine() {
 		while(true) {
-			distanceVector = GetDistanceToTarget (moveTarget);
+			distanceVector = GetDistanceToTarget (targeter.moveTarget);
 			lastLinearForce = GetDesiredVelocity (distanceVector);
 			this.rigidbody.AddForce (lastLinearForce, ForceMode.Force);
 			yield return new WaitForFixedUpdate();
---
+++ Assets/Scripts/World/KeyboardTargeter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyboardTargeter : MonoBehaviour {
+
+	public Vector3 moveTarget;
+	public Vector3 newTargetOffset;
+	public KeyboardConfiguration keyboardConfig;
+	
+	void Reset() {
+		keyboardConfig = this.GetComponent<KeyboardConfiguration>();
+	}
+
+	void Start () {
+		moveTarget = this.rigidbody.position;
+		StartCoroutine(checkInput());
+	}
+	
+	IEnumerator checkInput() {
+		while(true) {
+			newTargetOffset = Vector3.zero;
+			if (Input.GetKey (keyboardConfig.up)) {
+				newTargetOffset.z += .1f;
+			}
+			if (Input.GetKey (keyboardConfig.down)) {
+				newTargetOffset.z -= .1f;
+			}
+			if (Input.GetKey (keyboardConfig.left)) {
+				newTargetOffset.x -= .1f;
+			}
+			if (Input.GetKey (keyboardConfig.right)) {
+				newTargetOffset.x += .1f;
+			}
+			moveTarget = newTargetOffset + this.rigidbody.position;
+			yield return new WaitForFixedUpdate();
+		}
+	}
+}
