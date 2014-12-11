96e623e6d1ffbb8e11ac8171f7148d37c3a15c25
Nicholas Scheurich <nick@scheurich.me>
2014-12-11 15:07:12 -0600

    Ditto

---
+++ Assets/KeyboardConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardConfiguration : MonoBehaviour {
+
+	public KeyCode up;
+	public KeyCode down;
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode shoot;
+
+	[ContextMenu("Set WASD")]
+	void SetWASD() {
+		up    = KeyCode.W;
+		down  = KeyCode.S;
+		left  = KeyCode.A;
+		right = KeyCode.D;
+		shoot = KeyCode.Space;
+	}
+
+	[ContextMenu("Set Arrows")]
+	void SetArrows() {
+		up    = KeyCode.UpArrow;
+		down  = KeyCode.DownArrow;
+		left  = KeyCode.LeftArrow;
+		right = KeyCode.RightArrow;
+		shoot = KeyCode.RightControl;
+	}
+}
