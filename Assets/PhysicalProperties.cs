7295c57089c798b6434503e668e98b11930e0910
Nicholas Scheurich <nick@scheurich.me>
2014-12-16 16:16:56 -0600

    Add simple physical properties framework and sturdy WALLS

---
+++ Assets/PhysicalProperties.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PhysicalProperties : MonoBehaviour {
+	
+	public PhysicalPropertyTemplate propertiesTemplate;
+
+	// Start is called just before any of the
+	// Update methods is called the first time.
+	void Start () {
+		this.rigidbody.mass = propertiesTemplate.mass;
+	}
+	
+}
---
+++ Assets/PhysicalPropertyTemplate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicalPropertyTemplate : MonoBehaviour {
+
+	public float mass = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+}
