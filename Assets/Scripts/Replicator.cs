609693327d5df827b44573a993bce49b3811416a
Nicholas Scheurich <nick@scheurich.me>
2014-12-22 16:21:35 -0600

    Add Replicator

---
+++ Assets/Scripts/Replicator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Replicator : MonoBehaviour {
+
+	public float replicationInterval = 1.0f;
+	public Vector3 replicationAxes;
+	public bool replicate = false;
+	
+	void Start() {
+		StartCoroutine(Replicate());
+	}
+	
+	IEnumerator Replicate() {
+		while(true) {
+			yield return new WaitForSeconds(replicationInterval);
+			if (replicate) {
+				Vector3 newPosition = transform.position + replicationAxes;
+				GameObject o = (GameObject)Instantiate(gameObject, newPosition, transform.rotation);
+				o.GetComponent<Replicator>().replicate = false;
+			}
+		}
+	}
+}
